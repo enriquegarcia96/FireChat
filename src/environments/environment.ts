14e0b05a17d2e9f027db95a5674274a246e34318
enriquegarcia96 <enriquegarcia822@gmail.com>
2020-12-27 14:35:31 +0000

    Instalacion del angularFire2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAMZD72ivkg5upceOzejhkqOcJvrzL_Ftk",
+    authDomain: "firechat-613a5.firebaseapp.com",
+    projectId: "firechat-613a5",
+    storageBucket: "firechat-613a5.appspot.com",
+    messagingSenderId: "40341365394",
+    appId: "1:40341365394:web:3a3cf703426919b74afc4b",
+    measurementId: "G-SM2WHD21J1"
+  }
 };
 
 /*
