76256ba364b9b852f55bc813306b52c47e21eeca
enriquegarcia96 <enriquegarcia822@gmail.com>
2020-12-28 09:56:04 +0000

    cargar y agregar mensajes al chat uy desplegar mensajes

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,21 +1,53 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
+import { Mensaje } from '../interface/mensaje.interface';
+import { map } from 'rxjs/operators';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-  private itemsCollection: AngularFirestoreCollection<any>;
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
 
-  public chats: any[] = [];
+  public chats: Mensaje[] = [];
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private angularFirestore: AngularFirestore) { }
 
   // metodo
   cargarMensajes(){
 
-    this.itemsCollection = this.afs.collection<any>('chats');
+    this.itemsCollection = this.angularFirestore.collection<Mensaje>('chats', ref => ref.orderBy('fecha','desc').limit(5)); // ordeno los mensajes de forma asendente
 
-    return this.itemsCollection.valueChanges()
+    return this.itemsCollection.valueChanges().pipe(
+      map((mensajes: Mensaje[])=>{
+        console.log(mensajes);
+
+        this.chats = [];
+        for ( let mensaje of mensajes ){
+          this.chats.unshift( mensaje );
+        }
+        return this.chats;
+
+      })
+    )
   }
+
+  // agrega los mensajes a firebase
+  agregarMensaje( texto: string ){
+
+    // Objeto que le envio a firebase para grabarlo
+    let mensaje: Mensaje = {
+      nombre: 'Demo',
+      mensaje: texto,
+      fecha: new Date().getTime()
+    }
+
+    // inserto el mensaje a firebase
+    return this.itemsCollection.add( mensaje );
+
+  }
+
+
 }
