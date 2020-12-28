1dc8fd235e453a0420f00b8983d4e984d1fda490
enriquegarcia96 <enriquegarcia822@gmail.com>
2020-12-28 15:18:37 +0000

    problemas resueltos con compatibilidad

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAMZD72ivkg5upceOzejhkqOcJvrzL_Ftk",
+    authDomain: "firechat-613a5.firebaseapp.com",
+    projectId: "firechat-613a5",
+    storageBucket: "firechat-613a5.appspot.com",
+    messagingSenderId: "40341365394",
+    appId: "1:40341365394:web:3a3cf703426919b74afc4b",
+    measurementId: "G-SM2WHD21J1"
+  }
 };
